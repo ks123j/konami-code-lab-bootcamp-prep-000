22b7316dcd6b0a9bb71d090d19207be85d0ad8ca
Katy <komal.vadukul@gmail.com>
2017-11-25 15:02:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+function triggerKeyDown(which) {
+  const input = document.addEventListener('keydown', function(e) {
+    console.log(e.which);
+  });
+  document.body.dispatchEvent(input);
+}
+
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  function Konami_Code(e) {
+    const key = parseInt(e.detail || e.which);
+    for (let i = 0, l = code.length; i < l; i++) {
+      if (key === code[i]) {
+        triggerKeyDown(code[i])
+        alert (code[i]);
+        if (key === code.length){
+          i = 0;
+        }
+      } else {
+        i = 0;
+      }
+    }
+  }
 }
