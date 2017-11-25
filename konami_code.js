4c0504bdd1d18e78f50e505912c2a2c13a805676
Katy <komal.vadukul@gmail.com>
2017-11-25 17:02:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 
 function init() {
-  const input = document.addEventListener('keydown', Konami_code(e));
+  document.addEventListener('keydown', Konami_code(e));
   function Konami_Code(e) {
     const key = parseInt(e.detail || e.which);
     if (key === code[index]) {
